b3d80541bcb6a7442b84029974ede3494d373014
Kashyap Mahanta <kashyap959821@gmail.com>
2017-07-01 19:58:31 +0530

    final commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from numpy import random
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:kashyap@localhost/fb_bot'
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://wbklbekokfatzv:d6b8d560d30239b8c98fce3fe6e7115b147d7072cdc617f82706527644c5a6b3@ec2-23-21-158-253.compute-1.amazonaws.com:5432/d8jcgmv3mbh3q8"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://pommhsyqyqzeru:2711b3952a28db3a024f241c9fb06fb793fc0967052f60cf735c77f410ac2bb6@ec2-50-19-218-160.compute-1.amazonaws.com:5432/daocq4mn1fibj3"
 
 db = SQLAlchemy(app)
 
